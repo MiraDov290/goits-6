b698fbc3cf8a25ed8f25de84100ce182fa88a978
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-06-17 11:06:45 +0100

    changers

---
+++ script.js
@@ -0,0 +1,50 @@
+/* 2.<button>Click me</button>
+    <input type="text" id="input" /> */
+
+// const btn = document.querySelector('button');
+// const input = document.querySelector('#input');
+
+// btn.addEventListener('click', onClick)
+
+// function onClick() {
+//     alert(input.value);
+// }
+
+
+
+// 4.const btn = document.querySelector("button");
+// const paragraphs = document.querySelectorAll("p");
+// btn.addEventListener("click", onClick)
+// function onClick() {
+//     paragraphs.forEach((paragraph, idx) => {
+//         paragraph.textContent = idx + 1;
+//     });
+// }
+
+
+// 6.Напишіть інтерфейс, щоб створити список.
+//     Для кожного пункту:
+//     Запитуйте вміст у користувача за допомогою prompt. Створюйте пункт і додайте його до UL.
+//     Процес припиняється, коли користувач натискає ESC.
+//     Усі елементи мають створюватися динамічно. -->
+
+// const title = document.querySelector('h1');
+// const list = document.createElement('ul');
+
+// title.after(list);
+// const elmArr = [];
+// while (true) {
+//     const message = prompt("please text");
+//     if (!message) {
+//         break;
+//     }
+
+//     const li = document.createElement('li');
+//     li.textContent = message;
+//     elmArr.push(li);
+// }
+// list.append(...elmArr);
+
+
+
+
